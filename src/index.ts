464ac3394c306478f073ae3390abac1f2543522e
Joe McGrath <josephmcg01@gmail.com>
2023-12-01 21:26:34 +0900

    fix: properly bundle prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import eslintNext from "./eslint.next";
 import eslintReact from "./eslint.react.vite";
+import prettierConfig from "./prettier";
 
-export { eslintNext, eslintReact };
+export { eslintNext, eslintReact, prettierConfig };
