a0f85a92ef2eb29b32792fa3ed4a3309c7f20234
Joe McGrath <josephmcg01@gmail.com>
2024-01-30 18:28:52 +0900

    fix(eslint): exports, allows for proper plugin extension

--- packages/eslint-config-react/tsup.config.ts
+++ packages/eslint-config-react/tsup.config.ts
@@ -9,4 +9,11 @@ export default defineConfig({
   entry: ['index.ts'],
   format: ['cjs'],
   outDir: 'dist',
+  esbuildOptions: (options) => {
+    options.footer = {
+      // builds package with a default export, since that's how ESLint expects plugins to look.
+      // ? https://github.com/egoist/tsup/issues/572#issuecomment-1060599574
+      js: 'module.exports = module.exports.default;',
+    }
+  },
 })
