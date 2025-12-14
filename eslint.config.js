c7168c053cb1235537d02545c2c0307f212de58d
Joe McGrath <josephmcg01@gmail.com>
2025-12-14 11:52:17 +0900

    fix: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,42 @@
 // @ts-check
 
-import { defineConfig, globalIgnores } from 'eslint/config'
 import eslint from '@eslint/js'
+import { defineConfig, globalIgnores } from 'eslint/config'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import unusedImports from 'eslint-plugin-unused-imports'
+import tseslint from 'typescript-eslint'
 
 const config = defineConfig(
   globalIgnores(['.*', 'node_modules', 'dist']),
   eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
   // This disables the formatting rules in ESLint that Prettier is going to be responsible for handling.
   // Make sure it's always the last config, so it gets the chance to override other configs.
   eslintConfigPrettier,
+  {
+    languageOptions: {
+      ecmaVersion: 2024,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: 'module',
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+    },
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      // Import DX
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
 )
 
 export default config
