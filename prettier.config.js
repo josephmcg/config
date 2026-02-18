6f7876a7dadf36f199a5a8f809afa700a413020e
Joe McGrath <josephmcg01@gmail.com>
2026-02-18 23:58:25 +0900

    fix: remove goofy circular dependency

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,10 @@
-import config from '@josephmcg/prettier-config'
-
-export default config
+/** @type {import('prettier').Options} */
+export default {
+  arrowParens: 'always',
+  printWidth: 100,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  semi: false,
+  trailingComma: 'all',
+  tabWidth: 2,
+}
