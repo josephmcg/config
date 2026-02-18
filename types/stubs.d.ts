9d6671f52e4b3518ab5b17e8da68f940205c23b0
Joe McGrath <josephmcg01@gmail.com>
2026-02-18 23:29:46 +0900

    chore: remove unneeded stubs

--- types/stubs.d.ts
+++
@@ -1,8 +0,0 @@
-// https://github.com/eslint-community/eslint-plugin-eslint-comments/issues/214
-declare module '@eslint-community/eslint-plugin-eslint-comments/configs' {
-  import type { Linter } from 'eslint'
-  const plugin: {
-    recommended: Linter.Config
-  }
-  export = plugin
-}
